96dd2e1e90ad72f20a5ad7831028b1cac561af08
Abdul <aabdulmukhsin@yahoo.com>
2023-01-24 14:25:16 +0000

    spacing around cards

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return ( 
         <div className="flex justify-between px-20 py-10 bg-black text-white">
-            <p>Nini@gmail.com</p>
+            <p>Nino@gmail.com</p>
             <p>Copyright 2023 Nino, All Right Reserved</p>
         </div>
     );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from "../pokeman_banner.jpeg";
 
 const Header = () => {
     return (
-        <div className='bg-slate-200 p-10'>
-            <div>
-                <img src={Logo}  width="80px" className="rounded-full"/>
-            </div>
-
+        <div className='bg-slate-200'>
+            <Link to="/">
+                <div className="p-5">
+                    <img src={Logo} width="80px" className="rounded-full" />
+                </div>
+            </Link>
         </div>
     );
 }
